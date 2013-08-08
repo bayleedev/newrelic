428d0fb752753a8b9399507b9efaa6774c765344
Blaine Schmeisser <BlaineSch@gmail.com>
2013-08-07 22:35:37 -0500

    Commit initial code.

---
+++ test.cs
@@ -0,0 +1,5 @@
+class HelloWorld {
+	static void Main(string[] args) {
+		Console.WriteLine("Hello world!");
+	}
+}
---
+++ test.rb
@@ -0,0 +1,6 @@
+class HelloWorld
+  def say_hello!
+    puts "Hello world!"
+  end
+end
+HelloWorld.new.say_hello!
